338b7f8c1843fd0882eb87118bf4eeba9a8ffe2e
MRMokwa <matheus.mokwa@gmail.com>
2020-08-14 08:58:53 -0300

    Add @angular/material

--- projects/demo-app/src/app/app.module.ts
+++ projects/demo-app/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { DataViewModule } from 'data-view';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, DataViewModule],
+  imports: [BrowserModule, DataViewModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
