640a3ab94d76443b8834843478b29db735f26134
MRMokwa <matheus.mokwa@gmail.com>
2020-08-14 08:41:38 -0300

    Rename prefix from tds to app to remove conflicts

--- projects/demo-app/src/app/app.component.ts
+++ projects/demo-app/src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'tds-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'demo-app';
-}
+export class AppComponent {}
