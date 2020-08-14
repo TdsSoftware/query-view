8d6c12c3dfd633675f25e248c4c9df0c863b123c
MRMokwa <matheus.mokwa@gmail.com>
2020-08-14 11:06:16 -0300

    Add angular flex layout

--- projects/data-view/src/lib/data-view.module.ts
+++ projects/data-view/src/lib/data-view.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { DataViewComponent } from './data-view/data-view.component';
 import { TableComponent } from './table/table.component';
 import { TableDirective } from './table/table.directive';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [DataViewComponent, TableComponent, TableDirective],
-  imports: [],
+  imports: [FlexLayoutModule],
   exports: [DataViewComponent],
 })
 export class DataViewModule {}
