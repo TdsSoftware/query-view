8e848e59db1855bf86fddedc9fb63ab89c7fa7ce
MRMokwa <matheus.mokwa@gmail.com>
2020-08-18 11:44:06 -0300

    Add provider to state

--- projects/demo-app/src/app/clientes/data-view/data-view.component.ts
+++ projects/demo-app/src/app/clientes/data-view/data-view.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 
 import { TabelaComponent } from '../tabela/tabela.component';
 import { FiltroComponent } from '../filtro/filtro.component';
+import { DataViewStateService } from 'data-view';
 
 @Component({
   selector: 'app-data-view',
   templateUrl: './data-view.component.html',
   styleUrls: ['./data-view.component.scss'],
+  providers: [DataViewStateService],
 })
 export class DataViewComponent implements OnInit {
   table = TabelaComponent;
