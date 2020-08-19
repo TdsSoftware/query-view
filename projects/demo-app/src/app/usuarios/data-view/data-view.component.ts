712cdcde154a1fe83ded0e0235ab3435825e8221
MRMokwa <matheus.mokwa@gmail.com>
2020-08-19 09:09:05 -0300

    Fix providers

--- projects/demo-app/src/app/usuarios/data-view/data-view.component.ts
+++ projects/demo-app/src/app/usuarios/data-view/data-view.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 import { TabelaComponent } from '../../usuarios/tabela/tabela.component';
-import { DataViewStateService } from 'data-view';
+import { DataViewService } from 'data-view';
 
 @Component({
   selector: 'app-data-view',
   templateUrl: './data-view.component.html',
   styleUrls: ['./data-view.component.scss'],
-  providers: [DataViewStateService],
+  providers: [DataViewService],
 })
 export class DataViewComponent implements OnInit {
   table = TabelaComponent;
