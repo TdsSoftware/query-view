96ffe49d2f9c08abf043d81a3a13e8d4a8c9031c
MRMokwa <matheus.mokwa@gmail.com>
2020-08-26 17:25:43 -0300

    Replace dataview to queryview

--- projects/demo-app/src/app/usuarios/usuarios.module.ts
+++ projects/demo-app/src/app/usuarios/usuarios.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { DataViewModule } from 'data-view';
+
 import { UsuariosRoutingModule } from './usuarios-routing.module';
 import { DataViewComponent } from './data-view/data-view.component';
 import { SharedModule } from '../shared/shared.module';
@@ -8,6 +10,6 @@ import { TabelaComponent } from './tabela/tabela.component';
 
 @NgModule({
   declarations: [DataViewComponent, TabelaComponent],
-  imports: [CommonModule, UsuariosRoutingModule, SharedModule],
+  imports: [CommonModule, UsuariosRoutingModule, SharedModule, DataViewModule],
 })
 export class UsuariosModule {}
