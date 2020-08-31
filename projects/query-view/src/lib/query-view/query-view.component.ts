544f6f36298beba28724fc87af1b3fccb6d8e2b2
MRMokwa <matheus.mokwa@gmail.com>
2020-08-31 14:33:42 -0300

    Update mode of sidenav based on width

--- projects/query-view/src/lib/query-view/query-view.component.ts
+++ projects/query-view/src/lib/query-view/query-view.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input, Type } from '@angular/core';
+import { Component, Input, Type, ViewChild, ElementRef } from '@angular/core';
+import { MatDrawerContainer, MatDrawerMode } from '@angular/material/sidenav';
 
 import { QueryViewService } from '../query-view.service';
 import { FilterService } from '../filter/filter.service';
@@ -8,22 +9,32 @@ import { FilterService } from '../filter/filter.service';
   templateUrl: './query-view.component.html',
   styleUrls: ['./query-view.component.scss'],
 })
-export class QueryViewComponent implements OnInit {
+export class QueryViewComponent {
   @Input() title: string = '';
   @Input() table: Type<any>;
   @Input() filter: Type<any>;
   @Input() margin: string;
   @Input() elevation: number;
 
+  @ViewChild(MatDrawerContainer, { read: ElementRef })
+  matDrawerContainer: ElementRef;
+
   loading$ = this.queryViewService.loading$;
   filter$ = this.filterService.filter$;
+
+  mode: MatDrawerMode = 'side';
 
   constructor(
     private queryViewService: QueryViewService,
     private filterService: FilterService
   ) {}
 
-  ngOnInit(): void {}
+  ngDoCheck() {
+    this.mode =
+      this.matDrawerContainer?.nativeElement.clientWidth < 960
+        ? 'over'
+        : 'side';
+  }
 
   getElevation() {
     return this.margin ? `mat-elevation-z${this.elevation}` : null;
